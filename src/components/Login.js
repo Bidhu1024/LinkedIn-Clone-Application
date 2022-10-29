571a41cb99bfb37c44deeda23f692c89798c9973
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-10-29 06:44:08 +0530

    new feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,116 @@
-import React from 'react'
-import styled from 'styled-components'
-import loginLogo from '../Assets/login-logo.svg'
- const Login = () => {
+import React from "react";
+import styled from "styled-components";
+import loginLogo from "../Assets/login-logo.svg";
+import hero from '../Assets/login-hero.svg'
+const Login = () => {
   return (
-  <Container>
-    <Nav>
+    <Container>
+      <Nav>
         <a href="/">
-            <img src={loginLogo} alt="linkedinlogo" />
+          <img src={loginLogo} alt="linkedinlogo" />
         </a>
-    </Nav>
-  </Container>
-  )
-}
+        <div>
+          <Join>Join Now</Join>
+          <SignIn>Sign in</SignIn>
+        </div>
+      </Nav>
+      <Section>
+        <Hero>
+            <h1>Stay updated on your professional world</h1>
+            <img src={hero} alt="" />
+        </Hero>
+      </Section>
+    </Container>
+  );
+};
 const Container = styled.div`
-padding: 0;
+  padding: 0;
 `;
 const Nav = styled.div`
-max-width:1128px;
-margin: auto;
-padding: 12px 0 16px;
-display: flex;
-align-items: center;
-position: relative;
-justify-content: space-between;
-flex-wrap: nowrap;
-& > a{
+  max-width: 1128px;
+  margin: auto;
+  padding: 12px 0 16px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  & > a {
     width: 135px;
     height: 34px;
-    @media (max-width:768px){
-        padding:0 5px;
+    @media (max-width: 768px) {
+      padding: 0 5px;
+    }
+  }
+`;
+const Join = styled.a`
+  font-size: 20px;
+  padding: 10px 12px;
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.6);
+  margin-right: 12px;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.08);
+    color: rgba(0, 0, 0, 1);
+    text-decoration: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+`;
+const SignIn = styled.a`
+  box-shadow: inset 0 0 0 1px #0a66c2;
+  color: #0a66c2;
+  border-radius: 24px;
+  transition-duration: 167ms;
+  font-size: 1rem;
+  font-weight: 600;
+  line-height: 40px;
+  padding: 10px 24px;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  &:hover {
+    background-color: rgba(112, 181, 249, 0.15);
+    cursor: pointer;
+    color: #0a66c2;
+    text-decoration: none;
+  }
+`;
+const Section = styled.section`
+  display: flex;
+  align-content: start;
+  min-height: 700px;
+  padding-bottom: 138px;
+  padding-top: 40px;
+  padding: 60px 0;
+  position: relative;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1128px;
+  align-items: center;
+  margin: auto;
+  @media (max-width: 768px) {
+    margin: auto;
+    min-height: 0px;
+  }
+`;
+const Hero = styled.div`
+width: 100%;
+h1{
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media(max-width:768){
+        text-align: center;
+        font-size: 20px;
+        width: 100%;
+        line-height: 2;
     }
 }
-`;
+img{
 
+}
+`;
 export default Login;
